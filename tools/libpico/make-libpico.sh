3ddfb672495be09ca7e2d11a49a8b9fbe3fa1065
Goran Mišković <schkovich@users.noreply.github.com>
2025-05-21 09:59:42 +0200

    feat: configure UART as stdio for RP2040 - Enable UART for stdio (PICO_STDIO_UART=1) - Disable USB for stdio (PICO_STDIO_USB=0)

--- tools/libpico/make-libpico.sh
+++ tools/libpico/make-libpico.sh
@@ -11,7 +11,10 @@ export PATH="$(cd ../../system/picotool; pwd):$PATH"
 rm -rf build-rp2040
 mkdir build-rp2040
 cd build-rp2040
-CPU=rp2040 cmake ..
+CPU=rp2040 cmake \
+          -DPICO_STDIO_UART=1 \
+          -DPICO_STDIO_USB=0 \
+          ..
 make -j
 
 rm -rf boot
